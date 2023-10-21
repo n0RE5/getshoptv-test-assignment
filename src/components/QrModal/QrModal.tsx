2a5beaf043283822005b3bef84c5219b2bc6509e
Your Name <you@example.com>
2023-10-21 14:01:50 +0300

    add: qrcode modal

---
+++ src/components/QrModal/QrModal.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useEffect } from 'react'
+import { usePhoneModalStore } from '../../store/PhoneModalStore';
+import styles from './QrModal.module.scss';
+import qrcode from '../../assets/qr.svg'
+import { useQrModalStore } from '../../store/QrModalStore';
+
+const QrModal = () => {
+    const isActive = useQrModalStore(state => state.isActive)
+    const switchQrModalState = useQrModalStore(state => state.switchState)
+    const switchPhoneModalState = usePhoneModalStore(state => state.switchState)
+
+    const handleClick = useCallback(() => {
+        switchQrModalState(false)
+        switchPhoneModalState(true)
+    }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            switchQrModalState(true)
+        }, 5000)
+    }, [])
+
+    if(!isActive) {
+        return null
+    }
+
+    return (
+        <div className={styles.qrmodal}>
+            <div className={styles.qrmodal_w}>
+                <h1 className={styles.qrmodal_text}>ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША!</h1>
+                <h2 className={styles.qrmodal_text}>ПОДАРИТЕ ЕМУ СОБАКУ!</h2>
+                <div className={styles.qrmodal_qr}>
+                    <img src={qrcode} alt="qr" />
+                </div>
+                <div className={styles.qrmodal_desc}>
+                    Сканируйте QR-код 
+                    <br />
+                    или нажмите ОК
+                </div>
+                <button onClick={handleClick} className={styles.qrmodal_button}>OK</button>
+            </div>
+        </div>
+    );
+}
+
+export default QrModal;
