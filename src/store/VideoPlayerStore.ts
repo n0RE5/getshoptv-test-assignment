2bf421dc7b26b2531b32abfb8c3819229df30134
Your Name <you@example.com>
2023-10-21 12:21:18 +0300

    add: videoplayer store

---
+++ src/store/VideoPlayerStore.ts
@@ -0,0 +1,13 @@
+import {create} from 'zustand';
+
+interface VideoPlayerState {
+    isActive: boolean,
+    switchState: (arg0: boolean) => void
+}
+
+export const useVideoPlayerStore = create<VideoPlayerState>((set) => ({
+    isActive: true,
+    switchState: (active: boolean) => set(state => ({
+        isActive: active 
+    }))
+}))
