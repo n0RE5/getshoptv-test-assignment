a2d6ca64d36ebcc6584a2ae4d220bdc1fddb82aa
Your Name <you@example.com>
2023-10-21 11:37:59 +0300

    upd: global scss file import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
+import './styles/global.scss';
 
 function App() {
   return (
