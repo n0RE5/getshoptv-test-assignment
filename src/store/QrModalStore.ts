be8bbe2fd3a248101d080aed4a5b930ecaccca72
Your Name <you@example.com>
2023-10-21 14:02:03 +0300

    add: qrcodemodal store

---
+++ src/store/QrModalStore.ts
@@ -0,0 +1,13 @@
+import {create} from 'zustand';
+
+interface QrModalState {
+    isActive: boolean,
+    switchState: (arg0: boolean) => void
+}
+
+export const useQrModalStore = create<QrModalState>((set) => ({
+    isActive: false,
+    switchState: (active: boolean) => set(state => ({
+        isActive: active 
+    }))
+}))
