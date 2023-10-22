b930148d710200d90594ebebe6a602bee984722a
Your Name <you@example.com>
2023-10-22 10:56:21 +0300

    upd: changed div to button

--- src/components/KeyboardButton/KeyboardButton.tsx
+++ src/components/KeyboardButton/KeyboardButton.tsx
@@ -9,14 +9,14 @@ interface KeyboardButtonProps {
 
 const KeyboardButton: React.FC<KeyboardButtonProps> = React.memo(({ value, gridGrow, onClick }) => {
     return (
-        <div 
+        <button 
             className={styles.button} 
             data-grow={gridGrow}
             onClick={onClick}
             tabIndex={0}
         >
             {value}
-        </div>
+        </button>
     );
 })
 
